245faa21f4a0f5e00352a4d3b22a39df7ecaa34f
jareel11 <jareel.ireri@student.moringaschool.com>
2025-12-26 14:10:55 +0300

    Completed Flatburger Restaurant scope lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+// Global scope variable
+let burgers = ["Hamburger", "Cheeseburger"];
+
+// Global scope string
+let drink = "Milkshake";
+
+// Function-scoped variable
+function featuredDrink() {
+  let featured = "Vanilla Milkshake";
+  return featured;
+}
+
+// Block-scoped variable
+function addBurger() {
+  if (true) {
+    let newBurger = "Bacon Burger";
+    burgers.push(newBurger);
+  }
+}
+
+// Call the function so the block-scoped variable runs
+addBurger();
+console.log(burgers); // Output: ["Hamburger", "Cheeseburger", "Bacon Burger"]
