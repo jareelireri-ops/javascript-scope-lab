6a4e58ef4a33737db614ca225ed80bf2d0f04952
jareel11 <jareel.ireri@student.moringaschool.com>
2025-12-26 16:13:47 +0300

    Fixed Flatburger restaurant

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
-// Global scope variables
-const burgers = ["Hamburger", "Cheeseburger"];
-let featuredDrink = "Strawberry Milkshake";
+// 1. Declare burgers
+const burgers = ['Hamburger', 'Cheeseburger'];
 
-// Function to add burgers
+// 2. Declare featuredDrink
+let featuredDrink = 'Strawberry Milkshake';
+
+// 3. Function to add Flatburger
 function addBurger() {
-  const newBurger = "Flatburger"; // function-scoped variable
-  burgers.push(newBurger);
+    const newBurger = 'Flatburger';
+    burgers.push(newBurger);
+}
 
-  if (true) {
-    const anotherNewBurger = "Maple Bacon Burger"; // block-scoped variable
+// 4. If statement to add Maple Bacon Burger
+if (true) {
+    const anotherNewBurger = 'Maple Bacon Burger';
     burgers.push(anotherNewBurger);
-  }
 }
 
-// Function to change the featured drink
+// 5. Function to change featured drink
 function changeFeaturedDrink() {
-  featuredDrink = "The JavaShake";
+    featuredDrink = 'The JavaShake';
 }
 
-// Call functions so changes happen
+// 6. Call the functions so tests see changes
 addBurger();
 changeFeaturedDrink();
+
+// 7. Optional: show final state (just for verification)
+console.log(burgers);          // ['Hamburger', 'Cheeseburger', 'Maple Bacon Burger', 'Flatburger']
+console.log(featuredDrink);    // 'The JavaShake'
