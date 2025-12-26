8585a23e4e29ae7f3f1003ecfa4c6c18e5198d18
jareel11 <jareel.ireri@student.moringaschool.com>
2025-12-26 16:23:05 +0300

    Reviewed Flatburger Lab

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
-// Declare burgers
-const burgers = ["Hamburger", "Cheeseburger"];
-
-// Declare featuredDrink
+const burgers = ["Hamburger","Cheeseburger"];
 let featuredDrink = "Strawberry Milkshake";
 
-// Function to add Flatburger
 function addBurger() {
   const newBurger = "Flatburger";
   burgers.push(newBurger);
 }
 
-// If statement with true condition
-if (true) {
+if(true){
   const anotherNewBurger = "Maple Bacon Burger";
   burgers.push(anotherNewBurger);
 }
 
-// Function to change featured drink
 function changeFeaturedDrink() {
   featuredDrink = "The JavaShake";
 }
